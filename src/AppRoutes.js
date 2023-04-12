1dfc4ff4a4aab825eceff90e5a632f12e45f1bfd
David Diaz Clifton <daviddiazclifton@Davids-MacBook-Pro.local>
2023-04-12 20:42:12 +0200

    added a improved version of prototype for Social Page

--- src/AppRoutes.js
+++ src/AppRoutes.js
@@ -1,3 +1,4 @@
+// Importing required libraries and components
 import React, { useState, useEffect } from 'react';
 import { Route, Routes } from 'react-router-dom';
 
@@ -19,13 +20,24 @@ function AppRoutes() {
     localStorage.setItem('todos', JSON.stringify(todos));
   }, [todos]);
 
+  // Dummy users data
+  const users = [
+    { id: 1, name: 'Alice', icon: '/path/to/icon1.png', goal: 'Learn React' },
+    { id: 2, name: 'Bob', icon: '/path/to/icon2.png', goal: 'Learn Node.js' },
+    // ...
+  ];
+
+  const currentUser = {
+    id: 1, // Set the logged-in user's id
+  };
+
   return (
     <Routes>
       <Route path="/" element={<HomePage />} index={true} />
       <Route path="/settings" element={<SettingsPage />} />
       <Route path="/statistics" element={<StatisticsPage />} />
       <Route path="/weekly" element={<WeeklyPage todos={todos} setTodos={setTodos} />} />
-      <Route path="/social" element={<SocialPage />} />
+      <Route path="/social" element={<SocialPage users={users} currentUser={currentUser} />} />
       <Route path="/newTask" element={<NewTask todos={todos} setTodos={setTodos} />} />
       <Route path="/today" element={<TodayPage todos={todos} />} />
     </Routes>
--- src/SocialPage.js
+++ src/SocialPage.js
@@ -1,17 +1,60 @@
-import React from "react";
-import NavBar from "./NavBar/NavBar";
+import React from 'react';
+import Navbar from './NavBar/NavBar';
+import { Container, Row, Col, Card, Button } from 'react-bootstrap';
 
-// import './SocialPage.css';
+const offlineSuggestions = [
+  'Go for a walk with a friend',
+  'Attend a local meetup event',
+  'Visit a museum or art gallery',
+  'Join a sports club or group exercise class',
+  'Volunteer at a local organization',
+  'Take a cooking class',
+  'Meditate for 10 minutes daily',
+  'Keep a gratitude journal',
+  'Try deep breathing exercises',
+  'Get a full night of sleep',
+];
 
+function getRandomSuggestion() {
+  const index = Math.floor(Math.random() * offlineSuggestions.length);
+  return offlineSuggestions[index];
+}
 
-function SocialPage() {
+function SocialPage({ users, currentUser }) {
   return (
-    
-    <div>
-      <NavBar />
-      <h1>Social Page</h1>
-      <p>This is the social page</p>
-    </div>
+    <>
+      <Navbar />
+      <Container className="my-5">
+        <Row className="gx-5">
+          <Col md={6}>
+            <h2>Meaningful Interactions</h2>
+            <Row className="gx-5">
+              {users.map((user) => (
+                <Col md={4} key={user.id}>
+                  <Card className="mb-4">
+                    <Card.Img variant="top" src={user.icon} />
+                    <Card.Body>
+                      <Card.Title>{user.name}</Card.Title>
+                      <Card.Text>Goal: {user.goal}</Card.Text>
+                      <Button variant="primary">Connect</Button>
+                    </Card.Body>
+                  </Card>
+                </Col>
+              ))}
+            </Row>
+          </Col>
+          <Col md={6}>
+            <h2>Offline Interactions & Wellbeing Advice</h2>
+            <Card className="mb-4">
+              <Card.Body>
+                <Card.Title>Wellbeing Advice</Card.Title>
+                <Card.Text>{getRandomSuggestion()}</Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+    </>
   );
 }
 
